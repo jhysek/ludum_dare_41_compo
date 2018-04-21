5f6bcdefad08b6c6b990b49cf496c8e84f87d4fb
Jiri Hysek <kontakt@jirihysek.cz>
2018-04-21 08:49:39 +0200

    Initial import

---
+++ Scripts/Ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+func _ready():
+	set_process_input(true)
+	
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
